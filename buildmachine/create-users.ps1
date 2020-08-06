1a37b84b05f71ad2f70523d9bc5558410cee3449
Stano Peťko <stano@stanopetko.eu>
2020-08-06 13:16:03 +0200

    Update build machine info

---
+++ buildmachine/create-users.ps1
@@ -0,0 +1,31 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding()]
+param (
+	[Parameter()][string]$UserName,
+	[Parameter()][int]$NumberOfUsers
+)
+
+$userNameMaxLength = 15
+if ([string]::IsNullOrWhiteSpace($UserName)) {
+	$UserName = Read-Host -Prompt 'User name'
+}
+if (-not ($UserName -match "^[a-z0-9_-]{1,$userNameMaxLength}$")) {
+	throw "Invalid user name. It must be a string containig only letters, numbers, underscore (_) and hyphen (-) and its maximum length can be $userNameMaxLength characters."
+}
+if ($NumberOfUsers -lt 1) {
+	$NumberOfUsers = [int](Read-Host -Prompt 'Number of users')
+	if ($NumberOfUsers -lt 1) {
+		throw "Invalid number of users. It must be number greater or equal to 1."
+	}
+}
+else {
+	Write-Host "UserName = $UserName"
+	Write-Host "NumberOfUsers = $NumberOfUsers"
+	1..$NumberOfUsers | ForEach-Object {
+		$localUserName = "agent-$UserName-0$_"
+		$password = ConvertTo-SecureString -String $localUserName -AsPlainText
+		Write-Host "Creating user: $localUserName"
+		New-LocalUser -Name $localUserName -FullName $localUserName -Description "Account for DevOps build agent." -Password $password -PasswordNeverExpires -UserMayNotChangePassword -AccountNeverExpires
+	}
+}
