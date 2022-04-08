e980b8c5f5327f1dd1194e996c701d5ac2114777
Milan Martiniak <mino.martiniak@gmail.com>
2022-04-08 07:39:55 +0200

    Docker users and startup

---
+++ buildmachine/register-docker-start-schedule.ps1
@@ -0,0 +1,12 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding()]
+param (
+	[Parameter(Mandatory = $true)][string]$UserName,
+	[Parameter(Mandatory = $true)][SecureString]$UserPassword
+)
+
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$action = New-ScheduledTaskAction -Execute "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+$settings = New-ScheduledTaskSettingsSet -Compatibility Win8 -AllowStartIfOnBatteries
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "KROS - Start Docker on Start up" -Settings $settings -User $UserName -Password $UserPassword -RunLevel Highest
