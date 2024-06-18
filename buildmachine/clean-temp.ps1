8503762058dfe38c5514b046d848006e7cb35a2c
Stano Peťko <stano@stanopetko.eu>
2024-06-18 15:32:05 +0200

    Update clean-temp.ps1

--- buildmachine/clean-temp.ps1
+++ buildmachine/clean-temp.ps1
@@ -5,7 +5,7 @@ Delete old files and directories from Temp folders of all users.
 param (
 	[Parameter(Mandatory = $false)][string]$BaseFolder = "C:\Users",
 	[Parameter(Mandatory = $false)][string]$TempSubfolder = "AppData\Local\Temp",
-	[Parameter(Mandatory = $false)][int]$OlderThanDays = 4,
+	[Parameter(Mandatory = $false)][int]$OlderThanDays = 2,
 	[Parameter(Mandatory = $false)][switch]$SaveTranscript,
 	[Parameter(Mandatory = $false)][string]$TranscriptFile = "",
 	[Parameter(Mandatory = $false)][switch]$DryRun
@@ -21,30 +21,58 @@ if ($SaveTranscript) {
 	Start-Transcript -Path $TranscriptFile -UseMinimalHeader
 }
 
+if ($OlderThanDays -lt 1) {
+	$OlderThanDays = 1
+}
+
 $nowDate = [DateTime]::Now.ToString("d.M.yyyy")
 $nowTime = [DateTime]::Now.ToString("H:mm")
 
-Write-Output "Script started $nowDate at $nowTime"
-Write-Output "Using base folder '$BaseFolder'"
-Write-Output "Using temp subfolder '$TempSubfolder'"
-Write-Output "Cleaning folders from files and folders older than $OlderThanDays days:"
+Write-Host "Script started $nowDate at $nowTime"
+Write-Host "Using base folder '$BaseFolder'"
+Write-Host "Using temp subfolder '$TempSubfolder'"
+Write-Host "Cleaning folders from files and folders older than $OlderThanDays days:"
 if ($DryRun) {
-	Write-Output "  Dry run only, nothing will be really deleted."
+	Write-Host "  Dry run only, nothing will be really deleted."
 }
+
+Write-Host
 
 Get-ChildItem -Path $BaseFolder -Directory | ForEach-Object {
 	$userFolder = $_
 	$tempFolder = [IO.Path]::Join($userFolder.FullName, $TempSubfolder)
-	Write-Output $tempFolder
+	Write-Host $tempFolder
 	if (Test-Path -Path $tempFolder) {
 		Get-ChildItem -Path $tempFolder
 		| Where-Object {
-			$_.CreationTime -lt (Get-Date).AddDays(-$OlderThanDays)
+			$_.CreationTime -lt (Get-Date).Date.AddDays(-$OlderThanDays)
 		}
 		| ForEach-Object {
-			Write-Output "  $_"
+			Write-Host "  $_"
 			if (-not $DryRun) {
 				Remove-Item $_ -Recurse -ErrorAction Continue
+			}
+		}
+	}
+}
+
+Write-Host ""
+Write-Host "Remove empty folders"
+Get-ChildItem -Path $BaseFolder -Directory | ForEach-Object {
+	$userFolder = $_
+	$tempFolder = [IO.Path]::Join($userFolder.FullName, $TempSubfolder)
+	Write-Host $tempFolder
+	if (Test-Path -Path $tempFolder) {
+		Get-ChildItem -Path $tempFolder -Directory | ForEach-Object {
+			$tempSubFolder = $_
+			$count = (Get-ChildItem -Path $tempSubFolder).Count
+			if ($count -eq 0) {
+				Write-Host "$tempSubFolder - EMPTY → deleting"
+				if (-not $DryRun) {
+					Remove-Item $tempSubFolder -ErrorAction Continue
+				}
+			} else {
+				Write-Host "$tempSubFolder - NOT EMPTY"
 			}
 		}
 	}
