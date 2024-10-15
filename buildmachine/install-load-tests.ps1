5cfd1d577695f52881d3dfdaa5779ed8ba3b2fd3
Stano Gabo Peťko <stano@stanopetko.eu>
2024-10-15 15:42:27 +0200

    Update install-load-tests.ps1
    Use winget instead of Chocolatey.

--- buildmachine/install-load-tests.ps1
+++ buildmachine/install-load-tests.ps1
@@ -2,8 +2,7 @@
 
 [CmdletBinding()]
 param (
-    [Parameter()][string]$JMeterVersion = "5.4.1",
-    [Parameter()][string]$ToolsPath = "C:\tools",
+    [Parameter()][string]$ToolsPath = "C:/tools",
     [Parameter()][string]$PluginsList = "jpgc-graphs-basic,jpgc-casutg,jpgc-prmctl,websocket-samplers,jpgc-wsc"
 )
 
@@ -21,66 +20,55 @@ function AddToPath([string]$path) {
 	}
 }
 
-# Choco install
-Write-Host "Install software for load testing" -ForegroundColor Green
-choco install load-tests-buildmachine-packages.config --yes
+Write-Host "Install JMeter, this will also install Microsoft OpenJDK." -ForegroundColor Green
+winget install --id DEVCOM.JMeter --exact
 
-# Add JAVA folder to PATH
-Write-Host "Add JAVA folder to PATH" -ForegroundColor Green
-$javaHomeValue = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", [System.EnvironmentVariableTarget]::Machine)
-if ([string]::IsNullOrEmpty($javaHomeValue)) {
-    Write-Host "    'JAVA_HOME' is not set, so Java folder will not be set to the PATH."
-}
-else {
-    $javaBinPath = Join-Path -Path $javaHomeValue -ChildPath "bin"
-    AddToPath $javaBinPath
-}
-
-# Install JMeter
-Write-Host "Install JMeter" -ForegroundColor Green
-
-$jmeterPath = Join-Path -Path $ToolsPath -ChildPath "jmeter"
-$jmeterZipFile = Join-Path -Path $env:TEMP -ChildPath "jmeter.zip"
-$extractedFolder = Join-Path -Path $ToolsPath -ChildPath "apache-jmeter-$JMeterVersion"
-$pluginManagerFile = Join-Path -Path $jmeterPath -ChildPath "lib/ext/jmeter-plugins-manage.jar"
-$cmdRunnerFile = Join-Path -Path $jmeterPath -ChildPath "lib/cmdrunner-2.2.jar"
-
-## Delete target folder
-Write-Host "  Delete JMeter folder '$jmeterPath'" -ForegroundColor Yellow
+Write-Host "JMeter is installed in user scope, so copy it to the tools folder '$ToolsPath' and uninstall."
+$installedJMeterPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Programs" -AdditionalChildPath "JMeter"
+$jmeterPath = Join-Path -Path $ToolsPath -ChildPath "JMeter"
+Write-Host "  Remove JMeter folder if exist ($jmeterPath)."
 Remove-Item $jmeterPath -Recurse -ErrorAction Ignore
+Write-Host "  Copy local JMeter to tools folder ($installedJMeterPath → $jmeterPath)."
+Copy-Item $installedJMeterPath $jmeterPath -Recurse
 
-## Download JMeter
-Write-Host "  Download JMeter (v $JMeterVersion)" -ForegroundColor Yellow
-Invoke-WebRequest -Uri "https://downloads.apache.org/jmeter/binaries/apache-jmeter-$JMeterVersion.zip" -OutFile $jmeterZipFile
-
-## Extract JMeter
-Write-Host "  Extract downloaded JMeter" -ForegroundColor Green
-Expand-Archive -Path $jmeterZipFile -DestinationPath $ToolsPath
-Rename-Item $extractedFolder $jmeterPath
+Write-Host "  Uninstall local JMeter."
+winget uninstall --id DEVCOM.JMeter
 
 $jmeterBinPath = Join-Path -Path $jmeterPath -ChildPath "bin"
 AddToPath $jmeterBinPath
 
-## Delete source
-Write-Host "  Delete source file '$jmeterZipFile'" -ForegroundColor Yellow
-Remove-Item $jmeterZipFile -Recurse -ErrorAction Ignore
+# Add JAVA_HOME bin folder to Path. Installaton of OpenJDK adds this to PATH, but it is not visible in current
+# session, so I need to add it manually.
+$javaHome = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", "Machine")
+$javaHomeBin = Join-Path -Path $javaHome -ChildPath "bin"
+$env:Path = $env:Path + ";$javaHomeBin"
 
 # Install JMeter plugins
-## Download and install Plugin manager
+Write-Host
+
+$pluginsManagerFile = Join-Path -Path $jmeterPath -ChildPath "lib/ext/jmeter-plugins-manager.jar"
+$pluginsManagerUri = "https://jmeter-plugins.org/get"
 Write-Host "Install JMeter plugins" -ForegroundColor Green
-Write-Host "  Download and install Plugin manager" -ForegroundColor Yellow
-Invoke-WebRequest -Uri "https://jmeter-plugins.org/get/" -OutFile $pluginManagerFile
-Invoke-WebRequest -Uri "https://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar" -OutFile $cmdRunnerFile
+Write-Host "  Download plugin manager from '$pluginsManagerUri' to '$pluginsManagerFile'"
+Invoke-WebRequest -Uri $pluginsManagerUri -OutFile $pluginsManagerFile
 
-## Create PluginManagerCMDInstaller
-Write-Host "  Create PluginManagerCMDInstaller" -ForegroundColor Yellow
-java -cp $pluginManagerFile org.jmeterplugins.repository.PluginManagerCMDInstaller
+$cmdRunnerVersion = "2.3"
+$cmdRunnerFile = Join-Path -Path $jmeterPath -ChildPath "lib/cmdrunner-$cmdRunnerVersion.jar"
+$cmdRunnerUri = "https://repo1.maven.org/maven2/kg/apc/cmdrunner/$cmdRunnerVersion/cmdrunner-$cmdRunnerVersion.jar"
+Write-Host "  Download CMD runner from '$cmdRunnerUri' to '$cmdRunnerFile'"
+Invoke-WebRequest -Uri $cmdRunnerUri -OutFile $cmdRunnerFile
 
-Write-Host "  Install plugins" -ForegroundColor Yellow
-$pluginManagerCmd = Join-Path -Path $jmeterPath -ChildPath "bin/PluginsManagerCMD.bat"
-$cmd = "$pluginManagerCmd install $PluginsList"
-Invoke-Expression $cmd
+Write-Host "Create Plugins Manager command script"
+java -cp $pluginsManagerFile org.jmeterplugins.repository.PluginManagerCMDInstaller
 
+if ([System.Environment]::OSVersion.Platform -eq [System.PlatformID]::Win32NT) {
+	$pluginsManagerScript = "PluginsManagerCMD.bat"
+} else {
+	$pluginsManagerScript = "PluginsManagerCMD.sh"
+}
+$pluginManagerCmd = Join-Path -Path $jmeterPath -ChildPath "bin" -AdditionalChildPath $pluginsManagerScript
+Write-Host "  Install plugins using script '$pluginManagerCmd'" -ForegroundColor Yellow
+& $pluginManagerCmd install $PluginsList
 
 Write-Host
-Write-Host "  Everything is installed. Now run the 'configure.ps1' script." -ForegroundColor Yellow
+Write-Host "Everything is installed. Now run the 'configure.ps1' script." -ForegroundColor Green
